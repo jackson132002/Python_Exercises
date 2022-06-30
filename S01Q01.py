281b75ccb9040670bc720053af5af1a3e7c8e4a5
jackson132002 <72300636+jackson132002@users.noreply.github.com>
2022-06-30 10:27:38 +0530

    Update S01Q01.py

--- S01Q01.py
+++ S01Q01.py
@@ -1,10 +1,7 @@
+"""
+    Print "Hello <user_name>"
+"""
 #Q1
 name = input("Enter your name ")
 print("Hello", name)
 
-#QA1
-def name:
-    name1 = input("Enter your name ")
-    return name1
-ans = name
-print("Hello", ans,"!!!")
